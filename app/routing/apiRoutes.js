64dc404acc26a9e70cfb96ae93b3d666a7e9f4c6
unknown <eawest2@gmail.com>
2018-05-21 16:01:46 -0400

    90% completed

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -15,37 +15,42 @@ module.exports = function (app) {
 
     app.post("/api/users", function(req, res) {
         console.log("New Entry: " + JSON.stringify(req.body));
-        var newEntry = req.body;
+        
+        generateMatch(req, res);
+        });
+};
+
+function generateMatch (req, res){
+    var newEntry = req.body;
         var entryScores = newEntry.scores;
-        var totalDiffArr = []
+        var diffArr = []
         var lowest = 51;
         var matchName;
         var matchPhoto;
 
         for (var i = 0; i < users.length; i++) {
-            var totalDiff = 0;
+            var difference = 0;
             for (var j = 0; j < entryScores.length; j++) {
-            totalDiff += Math.abs(Number(users[i].scores[j]) - Number(entryScores[j]));
+            difference += Math.abs(Number(users[i].scores[j]) - Number(entryScores[j]));
             }
-            console.log(totalDiff);
+            console.log(difference);
 
-            totalDiffArr.push(totalDiff);
-            console.log("totalDiffArr: " + totalDiffArr);
+            diffArr.push(difference);
+            console.log("diffArr: " + diffArr);
 
-            if (totalDiff < lowest) {
-            lowest = totalDiff;
+            if (difference < lowest) {
+            lowest = difference;
             matchName = users[i].name;
             matchPhoto = users[i].photo;
             console.log(matchName);
             console.log(matchPhoto);
             }
         }
-        //***** When you send it back to the front end, it has to use the properties name and photo since that is what you are using in the front end when you do data.name and data.photo. It is expecting to find name and photo. It also has to be returned as an object *****
         var match = {
             name: matchName,
             photo: matchPhoto
-        }
-        console.log(match);
+        };
+
         res.json(match);
-        });
-};
+        console.log(match);
+}
