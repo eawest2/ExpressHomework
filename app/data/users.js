837edd6b39501b90ba1e2459ddbf1b284295efc7
unknown <eawest2@gmail.com>
2018-05-21 17:10:53 -0400

    90% completed

--- app/data/users.js
+++ app/data/users.js
@@ -1,5 +1,6 @@
 //Data storage
 
-var users = [];
+var users = [{"name":"Gimli, son of Gloin","photo":"https://en.wikipedia.org/wiki/Gimli_(Middle-earth)#/media/File:Gimli_With_Axe.jpg","scores":["4","4","2","3","3","2","4","4","2","4"]},
+{"name":"Thrall, son of Durotan","photo":"https://vignette.wikia.nocookie.net/wowwiki/images/0/00/Thrall_son_of_durotan_by_daerone-d8lu818.png/revision/latest/scale-to-width-down/277?cb=20150401124841","scores":["2","2","4","4","3","2","2","4","3","3"]}];
 
 module.exports = users;
